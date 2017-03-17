a6bd83395a8ba73e3bbf327bf26017b0bc625d32
janvanzeghbroeck <vanzeghb@gmail.com>
2017-03-17 14:40:39 -0600

    cron test script

---
+++ cron_test.py
@@ -0,0 +1,19 @@
+import boto
+import os
+
+# crontab -e
+# sudo service cron start
+
+# aws stuff
+aws_access_key = os.environ['AWS_ACCESS_KEY_ID']
+aws_access_secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+
+
+
+conn = boto.connect_s3(aws_access_key, aws_access_secret_key)
+
+
+bucket_name = 'urban-emoji-tweets'
+b = conn.get_bucket(bucket_name)
+file_object = b.new_key('tweets/cron_test.png')#where to save
+file_object.set_contents_from_filename('figures/1f913.png')
